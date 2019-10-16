5933e8ead14a92283f869d918c342fdeab1bc601
iFrosta <samgolovchenko@gmail.com>
2019-10-16 16:04:22 +0300

    php-1-6

--- php-1/php-1-6/homework/Gallery/engine/functions.php
+++ php-1/php-1-6/homework/Gallery/engine/functions.php
@@ -25,24 +25,43 @@ function prepareVariables($page, $action, $id)
       }
       break;
     case 'feedback':
-
-      if ($_GET['status'] == 1) {$params['error'] = "Отзыв добавлен!";}
-      if ($_GET['status'] == 2) {$params['error'] = "Отзыв удален!";}
-
-      if ($action == "add") {
-        $error = addFeedBack();
-        header("Location: /feedback/?status=1"); // Обязательно закрывающий /
-      }
-
-      if ($action == "delete") {
-        $error = deleteFeedback($id);
-        header("Location: /feedback/?status=2"); // Обязательно закрывающий /
-      }
-
+      doFeedbackAction($paramsm, $action, $id);
       $params['feedback'] = getAllFeedback();
+      $params['submitVal'] = 'Отправить';
       break;
   }
   return $params;
+}
+
+function doFeedbackAction(&$params, $action, $id){
+  if ($_GET['status'] == 1) {$params['error'] = "Отзыв добавлен!";}
+  if ($_GET['status'] == 2) {$params['error'] = "Отзыв удален!";}
+  if ($_GET['status'] == 3) {$params['error'] = "Отзыв отредактирован!";}
+
+  if ($action == "add") {
+    $error = addFeedBack();
+    header("Location: /feedback/?status=1"); // Обязательно закрывающий /
+  }
+
+  if ($action == "delete") {
+    $error = deleteFeedback($id);
+    header("Location: /feedback/?status=2"); // Обязательно закрывающий /
+  }
+
+  if ($action == "edit") {
+    $error = editFeedBack($id);
+    header("Location: /feedback/?status=3"); // Обязательно закрывающий /
+  }
+}
+
+function editFeedBack($id)
+{
+  $db = getDb();
+  $name = mysqli_real_escape_string($db, strip_tags(htmlspecialchars($_POST['name'])));
+  $message = mysqli_real_escape_string($db, strip_tags(htmlspecialchars($_POST['message'])));
+  $sql = "UPDATE `feedback` SET `id`={$id},`name`={$name},`feedback`={$message} WHERE 1";
+  var_dump($name, $message, $sql);
+  return executeQuery($sql);
 }
 
 function deleteFeedback($id)
