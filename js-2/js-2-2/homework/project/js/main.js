e5325b34d8a0423f15d4f3134818aec749b0cb5c
iFrosta <samgolovchenko@gmail.com>
2019-12-05 18:15:52 +0300

    js-2-2

--- js-2/js-2-2/homework/project/js/main.js
+++ js-2/js-2-2/homework/project/js/main.js
@@ -3,7 +3,7 @@ const products = [
   {id: 2, title: 'Mouse', price: 20, img:' https://img.mvideo.ru/Pdb/50048743b.jpg'},
   {id: 3, title: 'Keyboard', price: 200, img: 'https://www.logitechg.com/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-hero.png.imgw.1318.1318.jpeg'},
   {id: 4, title: 'Gamepad', price: 50, img: 'https://avatars.mds.yandex.net/get-mpic/1855911/img_id7980936131697456187.jpeg/9hq'},
-  {id: 5, title: 'Chair', price: 150}
+  {id: 5, title: 'Chair', price: 150, img: 'https://cdn11.bigcommerce.com/s-1ovkgbcja1/images/stencil/1280x1280/products/212/1965/PDP-TH-1__51615.1562964688.jpg?c=2?imbypass=on'}
 ];
 
 const renderProduct = (product, img = 'http://www.stoimen.com/wp-content/uploads/2011/10/question.mark_.jpg') => {
