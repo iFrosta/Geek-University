9d04d13b279c5de8164615d3e284788971570ba3
iFrosta <samgolovchenko@gmail.com>
2019-12-10 00:05:49 +0300

    js-2-2

--- js-2/js-2-2/homework/project/js/main.js
+++ js-2/js-2-2/homework/project/js/main.js
@@ -4,17 +4,17 @@ class ProductsList {
     this.goods = [];
     this.allProducts = [];
     this._fetchProducts();
-    this._calcSum();
     this._render();
+    console.log(this.calcSum());
   }
   _fetchProducts(){
     this.goods = [
       {id: 1, category: "Notebooks", title: 'Notebook', price: 2000, img: 'https://static.re-store.ru/upload/iblock/4fc/4fc2f36b75ade15f78baeefb04209361.jpg'},
-      {id: 2, category: "Mouses", title: 'Mouse', price: 20, img:' https://img.mvideo.ru/Pdb/50048743b.jpg'},
+      {id: 2, category: "Mouses", title: 'Mouse', price: 40, img:' https://img.mvideo.ru/Pdb/50048743b.jpg'},
       {id: 3, category: "Keyboards", title: 'Keyboard', price: 200, img: 'https://www.logitechg.com/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-hero.png.imgw.1318.1318.jpeg'},
       {id: 4, category: "Periphery", title: 'Gamepad', price: 50, img: 'https://avatars.mds.yandex.net/get-mpic/1855911/img_id7980936131697456187.jpeg/9hq'},
       {id: 5, category: "Chairs", title: 'Chair', price: 150, img: 'https://cdn11.bigcommerce.com/s-1ovkgbcja1/images/stencil/1280x1280/products/212/1965/PDP-TH-1__51615.1562964688.jpg?c=2?imbypass=on'},
-      {id: 6, category: "Periphery", title: 'Mouse pad', price: 10, img: 'https://images-na.ssl-images-amazon.com/images/I/A16NEfK2mZL._SL1500_.jpg'}
+      {id: 6, category: "Periphery", title: 'Mouse pad', price: 20, img: 'https://images-na.ssl-images-amazon.com/images/I/A16NEfK2mZL._SL1500_.jpg'}
     ];
   }
   _render(){
@@ -25,13 +25,8 @@ class ProductsList {
       block.insertAdjacentHTML('beforeend', productObj.render());
     }
   }
-  _calcSum(){
-    let sum = 0;
-    for (let product of this.goods){
-      sum += product['price'];
-    }
-    console.log('The cost of all goods: ' + sum + '$');
-    return sum;
+  calcSum(){
+    return this.allProducts.reduce((sum, item) => sum +=item.price, 0);
   }
 }
 
@@ -53,6 +48,23 @@ class ProductItem {
 }
 
 new ProductsList();
+
+class Cart {
+  constructor(container = '.cart') {
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+  }
+}
+
+class CartItem {
+  constructor(product){
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = product.img;
+  }
+}
 
 // const catalog = [
 //   {id: 1, title: 'Notebooks'},
