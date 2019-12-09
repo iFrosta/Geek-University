2f8e6b523efb6f6c5190a9305764576760b6d74f
iFrosta <samgolovchenko@gmail.com>
2019-12-09 23:07:21 +0300

    js-2-2

--- js-2/js-2-2/homework/project/js/main.js
+++ js-2/js-2-2/homework/project/js/main.js
@@ -1,24 +1,75 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 2000, img: 'https://static.re-store.ru/upload/iblock/4fc/4fc2f36b75ade15f78baeefb04209361.jpg'},
-  {id: 2, title: 'Mouse', price: 20, img:' https://img.mvideo.ru/Pdb/50048743b.jpg'},
-  {id: 3, title: 'Keyboard', price: 200, img: 'https://www.logitechg.com/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-hero.png.imgw.1318.1318.jpeg'},
-  {id: 4, title: 'Gamepad', price: 50, img: 'https://avatars.mds.yandex.net/get-mpic/1855911/img_id7980936131697456187.jpeg/9hq'},
-  {id: 5, title: 'Chair', price: 150, img: 'https://cdn11.bigcommerce.com/s-1ovkgbcja1/images/stencil/1280x1280/products/212/1965/PDP-TH-1__51615.1562964688.jpg?c=2?imbypass=on'}
-];
-
-const renderProduct = (product, img = 'http://www.stoimen.com/wp-content/uploads/2011/10/question.mark_.jpg') => {
-   return `<div class="product-item">
-        <h3>${product.title}</h3>
-        <p>${product.price}$</p>
-        <img src="${product.img}" alt="product-img">
-        <button class="buy-btn">Купить</button>
-    </div>`
-};
-
-const renderPage = list => {
-  for (let el of list) {
-    document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(el));
+class ProductsList {
+  constructor(container = '.products'){
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this._fetchProducts();
+    this._calcSum();
+    this._render();
   }
-};
+  _fetchProducts(){
+    this.goods = [
+      {id: 1, category: "Notebooks", title: 'Notebook', price: 2000, img: 'https://static.re-store.ru/upload/iblock/4fc/4fc2f36b75ade15f78baeefb04209361.jpg'},
+      {id: 2, category: "Mouses", title: 'Mouse', price: 20, img:' https://img.mvideo.ru/Pdb/50048743b.jpg'},
+      {id: 3, category: "Keyboards", title: 'Keyboard', price: 200, img: 'https://www.logitechg.com/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-hero.png.imgw.1318.1318.jpeg'},
+      {id: 4, category: "Periphery", title: 'Gamepad', price: 50, img: 'https://avatars.mds.yandex.net/get-mpic/1855911/img_id7980936131697456187.jpeg/9hq'},
+      {id: 5, category: "Chairs", title: 'Chair', price: 150, img: 'https://cdn11.bigcommerce.com/s-1ovkgbcja1/images/stencil/1280x1280/products/212/1965/PDP-TH-1__51615.1562964688.jpg?c=2?imbypass=on'},
+      {id: 6, category: "Periphery", title: 'Mouse pad', price: 10, img: 'https://images-na.ssl-images-amazon.com/images/I/A16NEfK2mZL._SL1500_.jpg'}
+    ];
+  }
+  _render(){
+    const block = document.querySelector(this.container);
+    for (let product of this.goods){
+      const productObj = new ProductItem(product);
+      this.allProducts.push(productObj);
+      block.insertAdjacentHTML('beforeend', productObj.render());
+    }
+  }
+  _calcSum(){
+    let sum = 0;
+    for (let product of this.goods){
+      sum += product['price'];
+    }
+    console.log('The cost of all goods: ' + sum + '$');
+    return sum;
+  }
+}
 
-renderPage(products);
+class ProductItem {
+  constructor(product, img  = 'http://www.stoimen.com/wp-content/uploads/2011/10/question.mark_.jpg'){
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = product.img;
+  }
+  render() {
+    return `<div class="product-item" data-id="item-${this.id}">
+            <h3>${this.title}</h3>
+            <p>${this.price}$</p>
+            <img src="${this.img}" alt="product-img">
+            <button class="buy-btn">Купить</button>
+          </div>`
+  }
+}
+
+new ProductsList();
+
+// const catalog = [
+//   {id: 1, title: 'Notebooks'},
+//   {id: 2, title: 'Keyboards'},
+//   {id: 3, title: 'Mouses'},
+//   {id: 4, title: 'Periphery'},
+//   {id: 5, title: 'Chairs'}
+// ];
+// const renderList = (catalog) => {
+//   return `<li class="catalog-item" id="list-${catalog.title}">${catalog.title}</li>`
+// };
+// for (let el of catalog) {
+//   document.querySelector('.list').insertAdjacentHTML('beforeend', renderList(el));
+// }
+// let catalogItems = document.querySelectorAll(".catalog-item");
+// catalogItems.forEach(function(elem) {
+//   elem.addEventListener("click", function() {
+//     console.log(this.id.replace('list-',''));
+//   });
+// });
