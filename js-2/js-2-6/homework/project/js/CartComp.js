e9d93d6803431c36dcb5471e59aa2e5f22a10b08
iFrosta <samgolovchenko@gmail.com>
2019-12-16 22:54:42 +0300

    js-2-6

--- js-2/js-2-6/homework/project/js/CartComp.js
+++ js-2/js-2-6/homework/project/js/CartComp.js
@@ -3,13 +3,24 @@ Vue.component('cart', {
   template: `<div v-show="visibility" class="cart-block">
               <div v-if="!cartItems.length" class="empty">Your Cart is empty! 🦊</div>
               <cart-item v-for="item of cartItems" :key="item.id_product" :cart-item="item" class="cart-item"></cart-item>
-            </div>`
+              <div class="line"></div>
+              <div v-if="cartItems.length" class="summary">SUBTOTAL: <strong>{{summary()}}</strong></div>
+            </div>`,
+  methods: {
+    summary() {
+      let sum = 0;
+      this.cartItems.forEach(item => {
+        sum += item.quantity * item.price;
+      });
+      return '$' + sum;
+    }
+  }
 });
 Vue.component('cart-item', {
   props: ['cartItem', 'img'],
   template: `<div class="cart-item">
                 <div class="product-bio">
-                  <img :src="cartItem.img" alt="img">
+                  <img :src="cartItem.img ? cartItem.img : 'https://cdn3.iconfinder.com/data/icons/project-management-32/48/24-512.png'" alt="img">
                   <div class="product-desc">
                     <p class="product-title">{{cartItem.product_name}}</p>
                     <p class="product-quantity">Quantity: {{cartItem.quantity}}</p>
