1127cd47e1c520eeb1b76a260f4620ed7bb30c3a
iFrosta <samgolovchenko@gmail.com>
2019-12-20 18:08:05 +0300

    js-2-7

--- js-2/js-2-7/homework/project_express/server/handler.js
+++ js-2/js-2-7/homework/project_express/server/handler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const cart = require('./cart');
+const logger = require('./logger');
 
 const actions = {
   add: cart.add,
@@ -12,11 +13,12 @@ let handler = (req, res, action, file) => {
     if (err) {
       res.sendStatus(404, JSON.stringify({result: 0, text: err}))
     } else {
-      let newCart = actions[action](JSON.parse(data), req);
+      let {name, newCart} = actions[action](JSON.parse(data), req);
       fs.writeFile(file, newCart, (err) => {
         if (err) {
           res.send('{"result": 0}');
         } else {
+          logger(name, action);
           res.send('{"result": 1}');
         }
       })
--- js-2/js-2-7/homework/project_express/server/logger.js
+++ js-2/js-2-7/homework/project_express/server/logger.js
@@ -2,8 +2,8 @@ const moment = require('moment');
 const fs = require('fs');
 
 const logger = (name, action) => {
-  fs.readFile('dist/server/db/stats.json', 'utf-8', (err, data) => {
-    if(err){
+  fs.readFile('server/db/stats.json', 'utf-8', (err, data) => {
+    if (err) {
       console.log('Can`t read file')
     } else {
       const stat = JSON.parse(data);
@@ -12,8 +12,8 @@ const logger = (name, action) => {
         prod_name: name,
         action: action
       });
-      fs.writeFile('dist/server/db/stats.json', JSON.stringify(stat, null, 4), (err) => {
-        if(err){
+      fs.writeFile('server/db/stats.json', JSON.stringify(stat, null, 4), (err) => {
+        if (err) {
           console.log('Can`t write file')
         }
       })
