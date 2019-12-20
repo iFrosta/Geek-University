7e3a0d01b58253e28f31bf09081c0297af0f188c
iFrosta <samgolovchenko@gmail.com>
2019-12-20 17:47:56 +0300

    js-2-7

--- js-2/js-2-7/homework/project_express/server/cart.js
+++ js-2/js-2-7/homework/project_express/server/cart.js
@@ -8,7 +8,7 @@ let change = (cart, req) => {
   return JSON.stringify(cart, null, 4);
 };
 let remove = (cart, req) => {
-  let find = cart.contents.find(el.id_product === +req.params.id);
+  let find = cart.contents.find(el => el.id_product === +req.params.id);
   cart.contents.splice(cart.contents.indexOf(find), 1);
   return JSON.stringify(cart, null, 4);
 };
